b75868960b4c00486eabe1e37a9988cd747a7cf0
Jan Willem Raats <jw.raats@gmail.com>
2015-10-28 21:54:19 +0100

    Initial Commit

---
+++ websockets-iphone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  websockets-iphone
+//
+//  Created by Jan Willem Raats on 28/10/15.
+//  Copyright © 2015 Jan Willem Raats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
