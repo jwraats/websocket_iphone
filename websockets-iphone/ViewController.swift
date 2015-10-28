aa80c88049ed872c702d5c751af021ac28e4af95
Jan Willem Raats <jw.raats@gmail.com>
2015-10-28 22:39:53 +0100

    The basic app for Iphone websockets

--- websockets-iphone/ViewController.swift
+++ websockets-iphone/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import SwiftWebSocket
 
 class ViewController: UIViewController {
-
+    var orientation: String!
+    let ws = WebSocket("wss://www.senzingyou.nl:1337")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "sendRotated", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        self.startUpSockets()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func sendRotated(){
+        if(UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation))
+        {
+            self.orientation = "landscape"
+            self.ws.send(self.orientation)
+        }
+        if(UIDeviceOrientationIsPortrait(UIDevice.currentDevice().orientation))
+        {
+           self.orientation = "portrait"
+           self.ws.send(self.orientation)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func startUpSockets(){
+        self.ws.event.open = {
+            self.view.backgroundColor = UIColor.orangeColor()
+        }
+        self.ws.event.close = { code, reason, clean in
+            self.view.backgroundColor = UIColor.orangeColor()
+        }
+        self.ws.event.error = { error in
+            self.view.backgroundColor = UIColor.orangeColor()
+        }
+        self.ws.event.message = { message in
+            if let text = message as? String {
+                if text == "isGreen: 'true'" {
+                    print("GROEN")
+                    self.view.backgroundColor = UIColor.greenColor()
+                }else{
+                    self.view.backgroundColor = UIColor.redColor()
+                }
+            }else{
+                self.view.backgroundColor = UIColor.orangeColor()
+            }
+        }
+    }
 
 }
 
